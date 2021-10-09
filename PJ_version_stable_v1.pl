72333fa8941f46ac59c46e14f621f7beb730acb5
NARWALSMAN <55153254+NARWALSMAN@users.noreply.github.com>
2021-10-09 16:38:45 +0200

    v2
    ajout present passée

--- PJ_version_stable_v1.pl
+++ PJ_version_stable_v1.pl
@@ -1,21 +1,21 @@
 %%% INFO-501, TP3
 %%% Pierre Hyvernat
 %%%Delifer Paul Guesdon
-%%% Lancez la "requ�te"
+%%% Lancez la "requete"
 %%% jouer.
 %%% pour commencer une partie !
 %
 
-% il faut d�clarer les pr�dicats "dynamiques" qui vont �tre modifi�s par le programme.
+% il faut declarer les predicats "dynamiques" qui vont etre modifies par le programme.
 :- dynamic position/2, position_courante/1.
 :- dynamic doloreane/2, temps_courant/1.
 :- dynamic position/3, position_courante/1.
-% on remet � jours les positions des objets et du joueur
+% on remet a jours les positions des objets et du joueur
 :- retractall(position(_, _)), retractall(position_courante(_)).
 :- retractall(doloreane(_, _)), retractall(temps_courant(_)).
 :- retractall(position_objets(_, _, _)), retractall(position_courante(_)).
 
-% on d�clare des op�rateurs, pour autoriser `prendre torche` au lieu de `prendre(torche)`
+% on declare des operateurs, pour autoriser `prendre torche` au lieu de `prendre(torche)`
 :- op(1000, fx, prendre).
 :- op(1000, fx, lacher).
 :- op(1000, fx, aller).
@@ -26,8 +26,8 @@ est_dans_liste(copies_systeme_dexploitation):-true,!.
 est_dans_liste(copies_pourraves):-true,!.
 est_dans_liste(copies_grammaire_automates):-true,!.
 est_dans_liste(copies_d_algorithmique):-true,!.
-est_dans_liste(_):-write("tu ne peux pas mettre �a!"), false, !.
-%Temporalit� du joueur. Ce predicat sert a definir dans quelle timeline le joueur se trouve
+est_dans_liste(_):-write("tu ne peux pas mettre ca!"), false, !.
+%Temporalite du joueur. Ce predicat sert a definir dans quelle timeline le joueur se trouve
 temps_courant(present).
 voyage(X,Temps):-
         temps_courant(Fromage),
@@ -39,7 +39,7 @@ voyage(X,Temps):-
         regarder, !.
 voyage(_,_):-write("la voiture n'est pas alimenté"), false, !.
 
-% position du joueur. Ce pr�dicat sera modifié au fur et é mesure de la partie (avec `retract` et `assert`)
+% position du joueur. Ce predicat sera modifié au fur et é mesure de la partie (avec `retract` et `assert`)
 position_courante(usmb_cours).
 
 %utiliser objet
@@ -55,7 +55,7 @@ utiliser(X):-
         !.
 
 
-% passages entre les diff�rent endroits du jeu
+% passages entre les different endroits du jeu
 
 %Present%
         %cours lama
@@ -71,12 +71,22 @@ utiliser(X):-
         passage(usmb_cours, sud, huitB).
         passage(huitB, nord,usmb_cours).
 
-% position des objets'
+% position des objets%
+%%
+%parchemin 1 2 3%
+position_objets(parchemin_1,lama,temps_courant(present)).
+position_objets(parchemin_2,huitB,temps_courant(futur)).
+position_objets(parchemin_3,quatrecanton,temps_courant(passe)).
+%%
+%code secret%
+position_objets(code_secret_1, lama, passe).
+position_objets(code_secret_2, polytech, present).
 %modele osi
 position_objets(modele_osi, lama, present).
-%Tri par bulle obtenu apr�s avoir parl� du modele osi a M.garet
+%Tri par bulle obtenu apres avoir parle du modele osi a Mgaret%
 position_objets(tri_par_bulle, lama, temps_courant(present)).
-
+%resultat%
+position_objets(resultat, polytech, temps_courant(futur)).
 %copies des eleves%
 position_objets(copies_pourraves,huitB, temps_courant(present)).
 position_objets(copies_partiels_logique,quattreCanton, temps_courant(present)).
@@ -88,8 +98,8 @@ position_objets(copies_partiels_logique,lama, temps_courant(futur)).
 position_objets(copies_dun_ancien_temps,huitB, temps_courant(futur)).
 % ramasser un objet
 prendre(X) :-
-        position_objets(X, en_main, temps_courant(present)),
-        write("Vous lavez d�j� !"), nl,
+        position(X, en_main),
+        write("Vous lavez deja !"), nl,
         !.
 
 %%%%%%%%%prendre et surcharges de prendre%%%%%%%%%%%
@@ -138,7 +148,7 @@ lacher(X) :-
         !.
 
 lacher(_) :-
-        write("Vous navez pas �a en votre possession !"), nl,
+        write("Vous navez pas ea en votre possession !"), nl,
         fail.
 
 
@@ -149,7 +159,7 @@ e :- aller(est).
 o :- aller(ouest).
 
 
-% d�placements
+% deplacements
 aller(Direction) :-
         position_courante(Ici),
         passage(Ici, Direction, La),
@@ -158,7 +168,7 @@ aller(Direction) :-
         regarder, !.
 
 aller(_) :-
-        write("Vous ne pouvez pas aller par l�."),
+        write("Vous ne pouvez pas aller par le."),
         fail.
 
 
@@ -170,7 +180,7 @@ regarder :-
         lister_objets(Place), nl.
 
 
-% afficher la liste des objets � lemplacement donn�
+% afficher la liste des objets e lemplacement donne
 lister_objets(Place) :-
         position_objets(X, Place, temps_courant(_)),
         write("Il y a "), write(X), write(" ici."), nl,
@@ -190,7 +200,7 @@ fin :-
 % affiche les instructions du jeu
 instructions :-
         nl,
-        write("Les commandes doivent �tre donn�es avec la syntaxe Prolog habituelle."), nl,
+        write("Les commandes doivent etre donnees avec la syntaxe Prolog habituelle."), nl,
         write("Les commandes existantes sont :"), nl,
         write("jouer.                   -- pour commencer une partie."), nl,
         write("n.  s.  e.  o.           -- pour aller dans cette direction (nord / sud / est / ouest)."), nl,
@@ -208,102 +218,142 @@ instructions :-
 jouer :-
         instructions,
         write("Catastrophe! Jean-Charles Marty revient du futur pour annoncer la nouvelle!"), nl,
-        write("Les licenses 3 ont foir�s leurs projets. L universit� est d�sormais discredit�e et"), nl,
-        write("est devenue la ris�e de la communaut� universitaire dans le domaine informatique"), nl,
-        write("Plus aucune entreprise ne veut proposer de projets aux futurs �tudiants de L3"), nl,
-        write("Votre mission, Si vous l acceptez est de parcourir les diff�rentes �poques"), nl,
-        write("afin de retrouver les algorithmes de tri permettant d �cr�mer la license."), nl,nl,
+        write("Les licenses 3 ont foires leurs projets. L universite est desormais discreditee et"), nl,
+        write("est devenue la risee de la communaute universitaire dans le domaine informatique"), nl,
+        write("Plus aucune entreprise ne veut proposer de projets aux futurs etudiants de L3"), nl,
+        write("Votre mission, Si vous l acceptez est de parcourir les differentes epoques"), nl,
+        write("afin de retrouver les algorithmes de tri permettant d ecremer la license."), nl,nl,
         regarder.
-% descriptions des emplacements du pass�
+% descriptions des emplacements du passe
 decrire(usmb_cours) :- temps_courant(passe),
-    write("SMEGMA"), nl,
-    write("vous appercevez marty, accompagn� de son fid�le compagnon le boussion-ardent, non loin de sa dolor�ane"), nl,!
+    write("vous vous retrouver en 1950"), nl,
+    write("la base est remplis de soldat"), nl,
+    !
     .
 
 decrire(lama) :- temps_courant(passe),position(copies_pourraves,vide),
     write("tri par bulle vide"), nl,
-    write(""), nl,!
+    write(""), nl,
+    !
     .
 decrire(lama) :- temps_courant(passe),
-    write("SMEGMA"), nl,
-    write(""), nl,!
+    write("vous vous trouvez dans le nouveau laboratoire scientifique dernier cri"), nl,
+    write("le flerons des mathématiques français"), nl,
+    write("vous parler a hyvernat l'étudiant qui revise ses cours de logique"), nl,
+    write("il vous dit qu'il est sur un projet secret"),nl,
+    write("ce projet pourrait rendre le plus null des étudiant en bosse des maths"), nl,
+    write("je connais que la moitier du code l'autre est caché avec un jeune disciple wental"), nl,
+    !
     .
 
 decrire(quattreCanton) :- temps_courant(passe),
-    write("SMEGMA"), nl,
-    write(""), nl,!
+    write("garet du passé zizi"), nl,
+    write(""), nl,
+    !
     .
 decrire(polytech) :- temps_courant(passe),
-    write("SMEGMA"), nl,
-    write(""), nl,!
+    write("ne me parle pas je cherche un algo mal fait pour ma machine a calculer"), nl,
+    !
+    .
+decrire(polytech) :- temps_courant(passe),position(zizi,en_main),
+    write("salut, mais cet un superbe algo de tri peut tu me le <<jiver>>"), nl,
+    write("ha merci maintenant plus qu'a attendre quelque temps"), nl,
+    !
     .
 
 % descriptions des emplacements du present
 decrire(usmb_cours) :- temps_courant(present),
     write("vous vous trouvez dans la cours de l usmb"), nl,
-    write("vous appercevez marty, accompagn� de son fid�le compagnon le boussion-ardent, non loin de sa dolor�ane"), nl,!.
+    write("vous appercevez marty, accompagne de son fidele compagnon le boussion-ardent, non loin de sa doloreane"), nl,!.
 
+ecrire(lama):- temps_courant(present),position(resultat,en_main),
+        write("vous avez les resultats bravos prenez ceci, ce parchemin vous aidera"), nl,
+        !
+        .
 
 decrire(lama):- temps_courant(present),
-        write("Vous arrivez dans ce prestigieux laboratoire. Vous appercevez M. Hyvernat devant la machine a caff�"), nl,
-        write("nn"),nl,!.
+        write("Vous arrivez dans ce prestigieux laboratoire. Vous appercevez M. Hyvernat devant la machine a caffe"), nl,
+        write("il arrive devant vous avec un papier a la main"),nl,
+        write("il dit: je sais ce que tu fait, je peu détecter les anomalies logiques"), nl,
+        write("tu joue a un jeu dangereux la fac n'as pas toujours été un lieu de paix"), nl,
+        write("prend cet algo puisque tu te lance dans cette aventure vas donner ca a un etudiant de polytech promo:1950"), nl,
+        !
+        .
 
 decrire(quattreCanton):-temps_courant(present),
-        write("quattreCanton"), nl,!.
+        write("vous entrez dans en 4canton, vous appercevez garet faisant peur au l1"), nl,
+        write("garet vous appercois et vous demande de venir"),nl,
+        write("je sais ce que tu cherche je le possedait autrefois mais je lai perdu"),nl,
+        write("si tu compte aller le chercher prend garde, je n'ai toujour été prof"),nl,
+        !
+        .
+
+decrire(huitB):-temps_courant(present),position(code_secret_1,en_main),position(code_secret_2,en_main),
+        write("comment a tu eu ce code!"), nl,
+        write("tu ne dois pas rester la"), nl,
+        write("je ne peux pas t'aider, t'as seule chance c'est de revenir plustard, bien plustard"), nl,
+        !
+        .
 
 decrire(huitB):-temps_courant(present),
-        write("huitB"), nl,!.
+        write("ne rentre pas j'ai du travail"), nl,
+        !
+        .
 
 decrire(polytech):-temps_courant(present),
-        write("polytech"), nl,!.
+        write("que fait tu ici?"), nl,
+        write("comment ca sauver le monde"), nl,
+        write("bon ecoute tu veux le code prend le moi je doit finir hive2"), nl,
+        !
+        .
 %%%%%%%%%%%Description des emplacements du futur
 decrire(usmb_cours) :- temps_courant(futur),
     write("vous etes dans la cours de l'usmb, cette cours est mal entretenu et des rat parcours la structure"), nl,
-    write("vous appercevez marty, accompagn� de son fid�le compagnon le boussion-ardent, non loin de sa dolor�ane"), nl,
+    write("vous appercevez marty, accompagne de son fidele compagnon le boussion-ardent, non loin de sa doloreane"), nl,
     !.
 
 decrire(lama):- temps_courant(futur),
-    write("Vous arrivez dans ce vetistute laboratoire. la machine a caff�est cass� "), nl,
+    write("Vous arrivez dans ce vetistute laboratoire. la machine a caffeest casse "), nl,
     write("M hyvernat est dans son bureau a corriger des copie de scratch"), nl,
     !.
 decrire(quattreCanton):- temps_courant(futur),
-    write("vous arriver dans les tr�s vieux batiment de l'univ"), nl,
+    write("vous arriver dans les tres vieux batiment de l'univ"), nl,
     write("mais pour une foix ces batiment sont raccord avec le reste de l'univ"), nl,
     write("on y appercois garret toujours en train de cermoner les l1:"), nl,
-    write("defaillant au tp, d�faillant au semestre d�faillant a l'ann�e, on est d'accord?"), nl,
+    write("defaillant au tp, defaillant au semestre defaillant a l'annee, on est d'accord?"), nl,
     write("...on est d'accord."),
     write("il vous interpelle et vous dit:"),
     write("zizi"),
     nl,
     !.
 decrire(huitB):- temps_courant(futur),
-    write("dernier grand batiment de cette universit� qui n'a pas encore �t� d�truit"), nl,
+    write("dernier grand batiment de cette universite qui n'a pas encore ete detruit"), nl,
     write("on y voit jol sur sont projet scret le fameux c+++"), nl,
     !.
 decrire(polytech):- temps_courant(futur),
     write("polytech est devenu un champ de ruine depuis les incidents du tp3 de logique"), nl,
-    write("wental est en train de d�velloper une applis mobile pour partir d'ici"), nl,
+    write("wental est en train de develloper une applis mobile pour partir d'ici"), nl,
     !.
 
 decrire(modele_osi):-position_courante(lama),temps_courant(present),
         write("un modele perdu dans les ages. Selon Maitre Bauzac"),nl,
         write("ce puissant arcane n'a pas ete implemente mais decrit les loi regissant l'internet"),nl,
         write("Ce modele pourrait interesser M.Garet"),nl.
-%%Il faudrait amener l'algo de tri par bulle � hyvernat%%
+%%Il faudrait amener lalgo de tri par bulle e hyvernat%%
 decrire(tri_par_bulle):-position_courante(lama),temps_courant(present),
-        write("Cet Algorithme de tri, bien que de complexit� constante n2"),nl,
-        write("doit etre utilis� avec jugeotte! Dans le cas contraire"),nl,
-        write("la situation risquerait de d�g�n�rer"),nl.
+        write("Cet Algorithme de tri, bien que de complexite constante n2"),nl,
+        write("doit etre utilise avec jugeotte! Dans le cas contraire"),nl,
+        write("la situation risquerait de degenerer"),nl.
 decrire(tri_par_bulle):-position(tri_par_bulle,vide),
         write("tu as mit le tri par bulle dans la dolorean"),nl,
         write("nous pouvons partir!!! en route!"),nl.
 
-%copies d'�tudiants%
+%copies detudiants%
 
 
-%copies du temps pr�sent%
+%copies du temps present%
 decrire(copies_partiels_logique):-
-        write("Ces copies sont remplies de symboles incompr�hensibles."),nl,
+        write("Ces copies sont remplies de symboles incomprehensibles."),nl,
         write("Absurde A(xor(non(non(AouBou(non(BetZxorC))))))"),nl,
         write("Aidez-Moi!").
 
